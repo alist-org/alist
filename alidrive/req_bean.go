8d74d070d4c17a49d9d17ea86fb789dc05f82768
lzz <lzz691997865@gmail.com>
2021-03-31 00:21:49 +0800

    Update req_bean.go

--- alidrive/req_bean.go
+++ alidrive/req_bean.go
@@ -57,6 +57,7 @@ type GetTokenReq struct {
 // refresh_token request bean
 type RefreshTokenReq struct {
 	RefreshToken string `json:"refresh_token"`
+	GrantType    string `json:"grant_type"`
 }
 
 // office_preview_url request bean
