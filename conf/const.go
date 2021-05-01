d693e27ec0fcc1b2790b3e957c8b4b9a95eb49e0
微凉 <927625802@qq.com>
2021-05-01 20:42:00 +0800

    :bookmark: release 1.0.4

--- conf/const.go
+++ conf/const.go
@@ -22,7 +22,7 @@ var (
 var Conf = new(Config)
 
 const (
-	VERSION = "v1.0.3"
+	VERSION = "v1.0.4"
 
 	ImageThumbnailProcess = "image/resize,w_50"
 	VideoThumbnailProcess = "video/snapshot,t_0,f_jpg,w_50"
