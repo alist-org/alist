d988f98b81f6a5a04713844273eb560f72e1020d
Xhofe <i@nn.ci>
2022-02-26 00:12:00 +0800

    :green_heart: fix upx

--- build.sh
+++ build.sh
@@ -67,8 +67,8 @@ BUILD() {
   mv alist-* build
   if [ "$1" != "release" ]; then
       cd build
-      upx -9 ./alist-linux
-      upx -9 ./alist-windows
+      upx -9 ./alist-linux*
+      upx -9 ./alist-windows*
       find . -type f -print0 | xargs -0 md5sum >md5.txt
       cat md5.txt
       cd ..
@@ -118,8 +118,8 @@ BUILD_MUSL() {
 
 RELEASE() {
   cd alist/build
-  upx -9 ./alist-linux
-  upx -9 ./alist-windows
+  upx -9 ./alist-linux*
+  upx -9 ./alist-windows*
   find . -type f -print0 | xargs -0 md5sum >md5.txt
   cat md5.txt
   mkdir compress
