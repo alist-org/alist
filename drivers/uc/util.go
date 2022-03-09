b71e1182aeae6830e282142d48a5e89a563faeef
Blackduck <lhx11187@qq.com>
2022-03-09 16:37:00 +0800

    Add files via upload

---
+++ drivers/uc/util.go
@@ -0,0 +1,31 @@
+package uc
+
+import (
+	"github.com/Xhofe/alist/conf"
+	"github.com/Xhofe/alist/model"
+	"github.com/Xhofe/alist/utils"
+	"path"
+	"time"
+)
+
+func getTime(t int64) *time.Time {
+	tm := time.UnixMilli(t)
+	//log.Debugln(tm)
+	return &tm
+}
+
+func (driver uc) formatFile(f *File) *model.File {
+	file := model.File{
+		Id:        f.Fid,
+		Name:      f.FileName,
+		Size:      f.Size,
+		Driver:    driver.Config().Name,
+		UpdatedAt: getTime(f.UpdatedAt),
+	}
+	if f.File {
+		file.Type = utils.GetFileType(path.Ext(f.FileName))
+	} else {
+		file.Type = conf.FOLDER
+	}
+	return &file
+}
