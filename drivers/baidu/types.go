3ad75e54cb754855c30c97a20dc18993a1d9ccae
foxxorcat <95907542+foxxorcat@users.noreply.github.com>
2022-04-16 14:52:36 +0800

    refactor(baidu): add a crack api of download
    * 修复百度网盘API文件大于20M问题
    
    * refactor: keep the official api
    
    Co-authored-by: Xhofe <i@nn.ci>

--- drivers/baidu/types.go
+++ drivers/baidu/types.go
@@ -74,6 +74,65 @@ type DownloadResp struct {
 	RequestId string `json:"request_id"`
 }
 
+type DownloadResp2 struct {
+	Errno int `json:"errno"`
+	Info  []struct {
+		//ExtentTinyint4 int `json:"extent_tinyint4"`
+		//ExtentTinyint1 int `json:"extent_tinyint1"`
+		//Bitmap string `json:"bitmap"`
+		//Category int `json:"category"`
+		//Isdir int `json:"isdir"`
+		//Videotag int `json:"videotag"`
+		Dlink string `json:"dlink"`
+		//OperID int64 `json:"oper_id"`
+		//PathMd5 int `json:"path_md5"`
+		//Wpfile int `json:"wpfile"`
+		//LocalMtime int `json:"local_mtime"`
+		/*Thumbs struct {
+			Icon string `json:"icon"`
+			URL3 string `json:"url3"`
+			URL2 string `json:"url2"`
+			URL1 string `json:"url1"`
+		} `json:"thumbs"`*/
+		//PlaySource int `json:"play_source"`
+		//Share int `json:"share"`
+		//FileKey string `json:"file_key"`
+		//Errno int `json:"errno"`
+		//LocalCtime int `json:"local_ctime"`
+		//Rotate int `json:"rotate"`
+		//Metadata time.Time `json:"metadata"`
+		//Height int `json:"height"`
+		//SampleRate int `json:"sample_rate"`
+		//Width int `json:"width"`
+		//OwnerType int `json:"owner_type"`
+		//Privacy int `json:"privacy"`
+		//ExtentInt3 int64 `json:"extent_int3"`
+		//RealCategory string `json:"real_category"`
+		//SrcLocation string `json:"src_location"`
+		//MetaInfo string `json:"meta_info"`
+		//ID string `json:"id"`
+		//Duration int `json:"duration"`
+		//FileSize string `json:"file_size"`
+		//Channels int `json:"channels"`
+		//UseSegment int `json:"use_segment"`
+		//ServerCtime int `json:"server_ctime"`
+		//Resolution string `json:"resolution"`
+		//OwnerID int `json:"owner_id"`
+		//ExtraInfo string `json:"extra_info"`
+		//Size int `json:"size"`
+		//FsID int64 `json:"fs_id"`
+		//ExtentTinyint3 int `json:"extent_tinyint3"`
+		//Md5 string `json:"md5"`
+		//Path string `json:"path"`
+		//FrameRate int `json:"frame_rate"`
+		//ExtentTinyint2 int `json:"extent_tinyint2"`
+		//ServerFilename string `json:"server_filename"`
+		//ServerMtime int `json:"server_mtime"`
+		//TkbindID int `json:"tkbind_id"`
+	} `json:"info"`
+	RequestID int64 `json:"request_id"`
+}
+
 type PrecreateResp struct {
 	Path       string `json:"path"`
 	Uploadid   string `json:"uploadid"`
