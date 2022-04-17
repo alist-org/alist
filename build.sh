f9533440c760bc3b62b6e91e1db6c5fdc77507c5
Xhofe <i@nn.ci>
2022-04-17 22:50:28 +0800

    build: cancel static link for glibc

--- build.sh
+++ build.sh
@@ -51,16 +51,8 @@ BUILD() {
   gitTag=$(git describe --long --tags --dirty --always)
   webTag=$(wget -qO- -t1 -T2 "https://api.github.com/repos/alist-org/alist-web/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
   echo "build version: $gitTag"
+
   ldflags="\
--w -s --extldflags '-static -fpic' \
--X 'github.com/Xhofe/alist/conf.BuiltAt=$builtAt' \
--X 'github.com/Xhofe/alist/conf.GoVersion=$goVersion' \
--X 'github.com/Xhofe/alist/conf.GitAuthor=$gitAuthor' \
--X 'github.com/Xhofe/alist/conf.GitCommit=$gitCommit' \
--X 'github.com/Xhofe/alist/conf.GitTag=$gitTag' \
--X 'github.com/Xhofe/alist/conf.WebTag=$webTag' \
-"
-  ldflagsDarwin="\
 -w -s \
 -X 'github.com/Xhofe/alist/conf.BuiltAt=$builtAt' \
 -X 'github.com/Xhofe/alist/conf.GoVersion=$goVersion' \
@@ -70,11 +62,9 @@ BUILD() {
 -X 'github.com/Xhofe/alist/conf.WebTag=$webTag' \
 "
   if [ "$1" == "release" ]; then
-    xgo -targets=linux/*,windows/* -out "$appName" -ldflags="$ldflags" -tags=jsoniter .
-    xgo -targets=darwin/* -out "$appName" -ldflags="$ldflagsDarwin" -tags=jsoniter .
+    xgo -out "$appName" -ldflags="$ldflags" -tags=jsoniter .
   else
-    xgo -targets=linux/amd64,windows/amd64 -out "$appName" -ldflags="$ldflags" -tags=jsoniter .
-    xgo -targets=darwin/amd64 -out "$appName" -ldflags="$ldflagsDarwin" -tags=jsoniter .
+    xgo -targets=linux/amd64,windows/amd64,darwin/amd64 -out "$appName" -ldflags="$ldflags" -tags=jsoniter .
   fi
   mkdir -p "build"
   mv alist-* build
