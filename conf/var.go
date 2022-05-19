9207eb69ee47ead481785c2885596b3066181932
Noah Hsu <i@nn.ci>
2022-05-19 15:31:40 +0800

    feat: add `m4v` to default video types (close #1114)

--- conf/var.go
+++ conf/var.go
@@ -40,7 +40,7 @@ var (
 		"go", "sh", "c", "cpp", "h", "hpp", "tsx", "vtt", "srt", "ass"}
 	DProxyTypes = []string{"m3u8"}
 	OfficeTypes = []string{"doc", "docx", "xls", "xlsx", "ppt", "pptx", "pdf"}
-	VideoTypes  = []string{"mp4", "mkv", "avi", "mov", "rmvb", "webm", "flv"}
+	VideoTypes  = []string{"mp4", "mkv", "avi", "mov", "rmvb", "webm", "flv", "m4v"}
 	AudioTypes  = []string{"mp3", "flac", "ogg", "m4a", "wav", "opus"}
 	ImageTypes  = []string{"jpg", "tiff", "jpeg", "png", "gif", "bmp", "svg", "ico", "swf", "webp"}
 )
