84eb978731b4480f3ceda8eb7951b3a48cb2cc5a
Noah Hsu <i@nn.ci>
2022-06-07 16:38:31 +0800

    feat: sort and proxy config

--- internal/model/account.go
+++ internal/model/account.go
@@ -7,4 +7,19 @@ type Account struct {
 	Driver      string `json:"driver"`
 	Status      string `json:"status"`
 	Addition    string `json:"addition"`
+	Sort
+	Proxy
+}
+
+type Sort struct {
+	OrderBy        string `json:"order_by"`
+	OrderDirection string `json:"order_direction"`
+	ExtractFolder  string `json:"extract_folder"`
+}
+
+type Proxy struct {
+	WebProxy     string `json:"web_proxy"`
+	WebdavProxy  bool   `json:"webdav_proxy"`
+	WebdavDirect bool   `json:"webdav_direct"`
+	DownProxyUrl string `json:"down_proxy_url"`
 }
