1143331b4d8738c95de2e6122216e0d642bb3d5b
Noah Hsu <i@nn.ci>
2022-06-14 17:19:43 +0800

    chore: `task` and `message` package

--- internal/task/task.go
+++ internal/task/task.go
@@ -1,1 +1,12 @@
+// manage task, such as file upload, file copy between accounts, offline download, etc.
 package task
+
+import "context"
+
+type Task struct {
+	Name     string
+	Func     func(context.Context) error
+	Status   string
+	Error    error
+	Progress int
+}
