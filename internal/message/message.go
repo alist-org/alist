a6e5edcf53a511081ddf8fd830f131e8597dc0ad
Noah Hsu <i@nn.ci>
2022-07-01 16:08:08 +0800

    chore: fix typo

--- internal/message/message.go
+++ internal/message/message.go
@@ -1,6 +1,8 @@
 package message
 
-type Messager interface {
+type Messenger interface {
 	Send(string, interface{}) error
+	WaitSend(string, interface{}) error
 	Receive(string) (string, error)
+	WaitReceive(string) (string, error)
 }
