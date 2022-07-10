8971a924f119c89f4afafa912aef4fb61ff98ad5
Noah Hsu <i@nn.ci>
2022-07-10 17:09:03 +0800

    fix: clear password while get current user

--- server/controllers/auth.go
+++ server/controllers/auth.go
@@ -62,6 +62,7 @@ func Login(c *gin.Context) {
 // if token is empty, return guest user
 func CurrentUser(c *gin.Context) {
 	user := c.MustGet("user").(*model.User)
-	user.Password = ""
-	common.SuccessResp(c, user)
+	userResp := *user
+	userResp.Password = ""
+	common.SuccessResp(c, userResp)
 }
