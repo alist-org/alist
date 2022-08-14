bd52df19266754ffa22078ed315345ecd4c4508e
Noah Hsu <i@nn.ci>
2022-08-14 15:38:22 +0800

    ci: use own proxy musl url

--- build.sh
+++ build.sh
@@ -81,7 +81,7 @@ BUILD() {
 }
 
 BUILD_MUSL() {
-  BASE="https://musl.cc/"
+  BASE="https://musl.nn.ci/"
   FILES=(x86_64-linux-musl-cross aarch64-linux-musl-cross arm-linux-musleabihf-cross mips-linux-musl-cross mips64-linux-musl-cross mips64el-linux-musl-cross mipsel-linux-musl-cross powerpc64le-linux-musl-cross s390x-linux-musl-cross)
   for i in "${FILES[@]}"; do
     url="${BASE}${i}.tgz"
