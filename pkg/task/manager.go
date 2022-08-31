454f563bce8363dc49efbedda29832764df6dabb
Noah Hsu <i@nn.ci>
2022-08-31 22:53:41 +0800

    fix: task id not update

--- pkg/task/manager.go
+++ pkg/task/manager.go
@@ -16,8 +16,8 @@ type Manager[K comparable] struct {
 
 func (tm *Manager[K]) Submit(task *Task[K]) K {
 	if tm.updateID != nil {
+		tm.updateID(&tm.curID)
 		task.ID = tm.curID
-		tm.updateID(&task.ID)
 	}
 	tm.tasks.Store(task.ID, task)
 	tm.do(task)
