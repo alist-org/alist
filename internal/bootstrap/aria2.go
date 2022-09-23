539c47bd3b6143cca13bd554af029848c39fb677
Noah Hsu <i@nn.ci>
2022-09-23 20:04:47 +0800

    chore: change log if aria2 not ready

--- internal/bootstrap/aria2.go
+++ internal/bootstrap/aria2.go
@@ -9,7 +9,8 @@ func InitAria2() {
 	go func() {
 		_, err := aria2.InitClient(2)
 		if err != nil {
-			utils.Log.Errorf("failed to init aria2 client: %+v", err)
+			//utils.Log.Errorf("failed to init aria2 client: %+v", err)
+			utils.Log.Infof("Aria2 not ready.")
 		}
 	}()
 }
