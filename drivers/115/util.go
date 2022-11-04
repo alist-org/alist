619a9aeb6ce25378679173dfb53cadced223c2ee
sheltonzhu <498220739@qq.com>
2022-11-04 21:16:52 +0800

    feat(115): add qrcode login (#2206)

--- drivers/115/util.go
+++ drivers/115/util.go
@@ -1,23 +1,36 @@
 package _115
 
 import (
+	"fmt"
+
 	"github.com/SheltonZhu/115driver/pkg/driver"
-	"github.com/alist-org/alist/v3/drivers/base"
+	"github.com/pkg/errors"
 )
 
 func (d *Pan115) login() error {
+	var err error
 	opts := []driver.Option{
-		driver.WithRestyClient(base.RestyClient),
 		driver.UA("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 115Browser/23.9.3.2 115disk/30.1.0"),
 	}
-
 	d.client = driver.New(opts...)
-
 	cr := &driver.Credential{}
-	if err := cr.FromCookie(d.Addition.Cookie); err != nil {
-		return err
+	if d.Addition.QRCodeToken != "" {
+		s := &driver.QRCodeSession{
+			UID: d.Addition.QRCodeToken,
+		}
+		if cr, err = d.client.QRCodeLogin(s); err != nil {
+			return errors.Wrap(err, "failed to login by qrcode")
+		}
+		d.Addition.Cookie = fmt.Sprintf("UID=%s;CID=%s;SEID=%s", cr.UID, cr.CID, cr.SEID)
+		d.Addition.QRCodeToken = ""
+	} else if d.Addition.Cookie != "" {
+		if err = cr.FromCookie(d.Addition.Cookie); err != nil {
+			return errors.Wrap(err, "failed to login by cookies")
+		}
+		d.client.ImportCredential(cr)
+	} else {
+		return errors.New("missing cookie or qrcode account")
 	}
-	d.client.ImportCredential(cr)
 	return d.client.LoginCheck()
 }
 
