e05e2fd663b2527c97c508370b755488f358aff6
Noah Hsu <i@nn.ci>
2022-11-08 20:37:42 +0800

    chore: change default timeout (close #2252)

--- drivers/base/client.go
+++ drivers/base/client.go
@@ -11,7 +11,7 @@ var NoRedirectClient *resty.Client
 var RestyClient = NewRestyClient()
 var HttpClient = &http.Client{}
 var UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
-var DefaultTimeout = time.Second * 10
+var DefaultTimeout = time.Second * 30
 
 func init() {
 	NoRedirectClient = resty.New().SetRedirectPolicy(
