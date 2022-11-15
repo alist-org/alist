3fbdf6f022b60d92f67842b22e22007cba92b713
BoYanZh <boyanzh233@gmail.com>
2022-11-15 10:51:32 +0800

    fix: resolve import cycle in alist v3 driver (close #2337 pr #2338)

--- drivers/alist_v3/types.go
+++ drivers/alist_v3/types.go
@@ -1,1 +1,45 @@
 package alist_v3
+
+import (
+	"time"
+
+	"github.com/alist-org/alist/v3/server/common"
+)
+
+type ListReq struct {
+	common.PageReq
+	Path     string `json:"path" form:"path"`
+	Password string `json:"password" form:"password"`
+	Refresh  bool   `json:"refresh"`
+}
+
+type ObjResp struct {
+	Name     string    `json:"name"`
+	Size     int64     `json:"size"`
+	IsDir    bool      `json:"is_dir"`
+	Modified time.Time `json:"modified"`
+	Sign     string    `json:"sign"`
+	Thumb    string    `json:"thumb"`
+	Type     int       `json:"type"`
+}
+
+type FsListResp struct {
+	Content  []ObjResp `json:"content"`
+	Total    int64     `json:"total"`
+	Readme   string    `json:"readme"`
+	Write    bool      `json:"write"`
+	Provider string    `json:"provider"`
+}
+
+type FsGetReq struct {
+	Path     string `json:"path" form:"path"`
+	Password string `json:"password" form:"password"`
+}
+
+type FsGetResp struct {
+	ObjResp
+	RawURL   string    `json:"raw_url"`
+	Readme   string    `json:"readme"`
+	Provider string    `json:"provider"`
+	Related  []ObjResp `json:"related"`
+}
