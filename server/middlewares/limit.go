bb0948d420ab08d3c608ed0e2ebd1fc3a129f48f
BoYanZh <boyanzh233@gmail.com>
2022-12-13 18:27:33 +0800

    feat: limit max connection count

---
+++ server/middlewares/limit.go
@@ -0,0 +1,16 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func MaxAllowed(n int) gin.HandlerFunc {
+	sem := make(chan struct{}, n)
+	acquire := func() { sem <- struct{}{} }
+	release := func() { <-sem }
+	return func(c *gin.Context) {
+		acquire()
+		defer release()
+		c.Next()
+	}
+}
