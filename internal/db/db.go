605bbca003828823e74388c11a72bb911eeb1ecb
BoYanZh <boyanzh233@gmail.com>
2022-12-16 14:31:59 +0800

    feat(search): use FULLTEXT index (#2716)

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/alist-org/alist/v3/internal/conf"
@@ -12,7 +13,21 @@ var db *gorm.DB
 
 func Init(d *gorm.DB) {
 	db = d
-	err := AutoMigrate(new(model.Storage), new(model.User), new(model.Meta), new(model.SettingItem), new(model.SearchNode))
+	var err error
+	switch conf.Conf.Database.Type {
+	case "sqlite3":
+		err = AutoMigrate(new(model.Storage), new(model.User), new(model.Meta), new(model.SettingItem), new(model.SearchNode))
+	case "mysql":
+		err = AutoMigrate(new(model.Storage), new(model.User), new(model.Meta), new(model.SettingItem), new(model.SearchNodeMySQL))
+	case "postgres":
+		err = AutoMigrate(new(model.Storage), new(model.User), new(model.Meta), new(model.SettingItem), new(model.SearchNode))
+		if err == nil {
+			db.Exec("CREATE EXTENSION pg_trgm;")
+			db.Exec("CREATE EXTENSION btree_gin;")
+			tableName := fmt.Sprintf("%ssearch_nodes", conf.Conf.Database.TablePrefix)
+			db.Exec(fmt.Sprintf("CREATE INDEX idx_%s_name ON %s USING GIN (name);", tableName, tableName))
+		}
+	}
 	if err != nil {
 		log.Fatalf("failed migrate database: %s", err.Error())
 	}
