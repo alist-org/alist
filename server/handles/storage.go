3ff9ccf275eb31c8d9d98ad9f5d5384b85a7bf6c
BoYanZh <boyanzh233@gmail.com>
2022-12-21 16:53:41 +0800

    feat: batch reload all storages (#2762)

--- server/handles/storage.go
+++ server/handles/storage.go
@@ -1,8 +1,10 @@
 package handles
 
 import (
+	"context"
 	"strconv"
 
+	"github.com/alist-org/alist/v3/internal/conf"
 	"github.com/alist-org/alist/v3/internal/db"
 	"github.com/alist-org/alist/v3/internal/model"
 	"github.com/alist-org/alist/v3/internal/op"
@@ -115,4 +117,36 @@ func GetStorage(c *gin.Context) {
 		return
 	}
 	common.SuccessResp(c, storage)
+}
+
+func LoadAllStorages(c *gin.Context) {
+	storages, err := db.GetEnabledStorages()
+	if err != nil {
+		log.Errorf("failed get enabled storages: %+v", err)
+		common.ErrorResp(c, err, 500, true)
+		return
+	}
+	conf.StoragesLoaded = false
+	go func(storages []model.Storage) {
+		for _, storage := range storages {
+			storageDriver, err := op.GetStorageByMountPath(storage.MountPath)
+			if err != nil {
+				log.Errorf("failed get storage driver: %+v", err)
+				continue
+			}
+			// drop the storage in the driver
+			if err := storageDriver.Drop(context.Background()); err != nil {
+				log.Errorf("failed drop storage: %+v", err)
+				continue
+			}
+			if err := op.LoadStorage(context.Background(), storage); err != nil {
+				log.Errorf("failed get enabled storages: %+v", err)
+				continue
+			}
+			log.Infof("success load storage: [%s], driver: [%s]",
+				storage.MountPath, storage.Driver)
+		}
+		conf.StoragesLoaded = true
+	}(storages)
+	common.SuccessResp(c)
 }
