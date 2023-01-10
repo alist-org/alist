b28db825e6e0561ce068083cc1c8b2fcbfc1a933
Noah Hsu <i@nn.ci>
2023-01-10 14:36:52 +0800

    refactor: optimization

--- drivers/seafile/driver.go
+++ drivers/seafile/driver.go
@@ -3,12 +3,15 @@ package seafile
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"path/filepath"
+	"strings"
 	"time"
 
-	"github.com/alist-org/alist/v3/drivers/base"
 	"github.com/alist-org/alist/v3/internal/driver"
 	"github.com/alist-org/alist/v3/internal/model"
+	"github.com/alist-org/alist/v3/pkg/utils"
+	"github.com/go-resty/resty/v2"
 )
 
 type Seafile struct {
@@ -27,22 +30,8 @@ func (d *Seafile) GetAddition() driver.Additional {
 }
 
 func (d *Seafile) Init(ctx context.Context) error {
-	authResp := &AuthTokenResp{}
-	_, err := base.RestyClient.R().
-		SetResult(authResp).
-		SetFormData(map[string]string{
-			"username": d.UserName,
-			"password": d.Password,
-		}).
-		Post(d.Address + "/api2/auth-token/")
-
-	if err != nil {
-		return err
-	}
-
-	d.authorization = fmt.Sprintf("Token %s", authResp.Token)
-
-	return nil
+	d.Address = strings.TrimSuffix(d.Address, "/")
+	return d.getToken()
 }
 
 func (d *Seafile) Drop(ctx context.Context) error {
@@ -51,24 +40,17 @@ func (d *Seafile) Drop(ctx context.Context) error {
 
 func (d *Seafile) List(ctx context.Context, dir model.Obj, args model.ListArgs) ([]model.Obj, error) {
 	path := dir.GetPath()
-
-	resp := &[]RepoDirItemResp{}
-	_, err := base.RestyClient.R().
-		SetResult(resp).
-		SetHeader("Authorization", d.authorization).
-		SetQueryParams(map[string]string{
+	var resp []RepoDirItemResp
+	_, err := d.request(http.MethodGet, fmt.Sprintf("/api2/repos/%s/dir/", d.Addition.RepoId), func(req *resty.Request) {
+		req.SetResult(&resp).SetQueryParams(map[string]string{
 			"p": path,
-		}).
-		Get(d.Address + fmt.Sprintf("/api2/repos/%s/dir/", d.Addition.RepoId))
-
+		})
+	})
 	if err != nil {
 		return nil, err
 	}
-
-	var files []model.Obj
-	for i := 0; i < len(*resp); i++ {
-		f := (*resp)[i]
-		file := model.ObjThumb{
+	return utils.SliceConvert(resp, func(f RepoDirItemResp) (model.Obj, error) {
+		return &model.ObjThumb{
 			Object: model.Object{
 				Name:     f.Name,
 				Modified: time.Unix(f.Modified, 0),
@@ -76,151 +58,102 @@ func (d *Seafile) List(ctx context.Context, dir model.Obj, args model.ListArgs) 
 				IsFolder: f.Type == "dir",
 			},
 			// Thumbnail: model.Thumbnail{Thumbnail: f.Thumb},
-		}
-		files = append(files, &file)
-	}
-
-	return files, nil
+		}, nil
+	})
 }
 
 func (d *Seafile) Link(ctx context.Context, file model.Obj, args model.LinkArgs) (*model.Link, error) {
-	resp, err := base.RestyClient.R().
-		SetHeader("Authorization", d.authorization).
-		SetQueryParams(map[string]string{
+	res, err := d.request(http.MethodGet, fmt.Sprintf("/api2/repos/%s/file/", d.Addition.RepoId), func(req *resty.Request) {
+		req.SetQueryParams(map[string]string{
 			"p":     file.GetPath(),
 			"reuse": "1",
-		}).
-		Get(d.Address + fmt.Sprintf("/api2/repos/%s/file/", d.Addition.RepoId))
-
+		})
+	})
 	if err != nil {
 		return nil, err
 	}
-
-	u := resp.String()
+	u := string(res)
 	u = u[1 : len(u)-1] // remove quotes
-
 	return &model.Link{URL: u}, nil
 }
 
 func (d *Seafile) MakeDir(ctx context.Context, parentDir model.Obj, dirName string) error {
-	resp, err := base.RestyClient.R().
-		SetHeader("Authorization", d.authorization).
-		SetQueryParams(map[string]string{
+	_, err := d.request(http.MethodPost, fmt.Sprintf("/api2/repos/%s/dir/", d.Addition.RepoId), func(req *resty.Request) {
+		req.SetQueryParams(map[string]string{
 			"p": filepath.Join(parentDir.GetPath(), dirName),
-		}).
-		SetFormData(map[string]string{
+		}).SetFormData(map[string]string{
 			"operation": "mkdir",
-		}).
-		Post(d.Address + fmt.Sprintf("/api2/repos/%s/dir/", d.Addition.RepoId))
-
-	if resp.StatusCode() == 201 {
-		return nil
-	}
-
+		})
+	})
 	return err
 }
 
 func (d *Seafile) Move(ctx context.Context, srcObj, dstDir model.Obj) error {
-	resp, err := base.NoRedirectClient.R().
-		SetHeader("Authorization", d.authorization).
-		SetQueryParams(map[string]string{
+	_, err := d.request(http.MethodPost, fmt.Sprintf("/api2/repos/%s/file/", d.Addition.RepoId), func(req *resty.Request) {
+		req.SetQueryParams(map[string]string{
 			"p": srcObj.GetPath(),
-		}).
-		SetFormData(map[string]string{
+		}).SetFormData(map[string]string{
 			"operation": "move",
 			"dst_repo":  d.Addition.RepoId,
 			"dst_dir":   dstDir.GetPath(),
-		}).
-		Post(d.Address + fmt.Sprintf("/api2/repos/%s/file/", d.Addition.RepoId))
-
-	if resp.StatusCode() == 301 {
-		return nil
-	}
-
+		})
+	})
 	return err
 }
 
 func (d *Seafile) Rename(ctx context.Context, srcObj model.Obj, newName string) error {
-	resp, err := base.NoRedirectClient.R().
-		SetHeader("Authorization", d.authorization).
-		SetQueryParams(map[string]string{
+	_, err := d.request(http.MethodPost, fmt.Sprintf("/api2/repos/%s/file/", d.Addition.RepoId), func(req *resty.Request) {
+		req.SetQueryParams(map[string]string{
 			"p": srcObj.GetPath(),
-		}).
-		SetFormData(map[string]string{
+		}).SetFormData(map[string]string{
 			"operation": "rename",
 			"newname":   newName,
-		}).
-		Post(d.Address + fmt.Sprintf("/api2/repos/%s/file/", d.Addition.RepoId))
-
-	if resp.StatusCode() == 301 {
-		return nil
-	}
-
+		})
+	})
 	return err
 }
 
 func (d *Seafile) Copy(ctx context.Context, srcObj, dstDir model.Obj) error {
-	resp, err := base.RestyClient.R().
-		SetHeader("Authorization", d.authorization).
-		SetQueryParams(map[string]string{
+	_, err := d.request(http.MethodPost, fmt.Sprintf("/api2/repos/%s/file/", d.Addition.RepoId), func(req *resty.Request) {
+		req.SetQueryParams(map[string]string{
 			"p": srcObj.GetPath(),
-		}).
-		SetFormData(map[string]string{
+		}).SetFormData(map[string]string{
 			"operation": "copy",
 			"dst_repo":  d.Addition.RepoId,
 			"dst_dir":   dstDir.GetPath(),
-		}).
-		Post(d.Address + fmt.Sprintf("/api2/repos/%s/file/", d.Addition.RepoId))
-
-	if resp.StatusCode() == 200 {
-		return nil
-	}
-
+		})
+	})
 	return err
 }
 
 func (d *Seafile) Remove(ctx context.Context, obj model.Obj) error {
-	resp, err := base.RestyClient.R().
-		SetHeader("Authorization", d.authorization).
-		SetQueryParams(map[string]string{
+	_, err := d.request(http.MethodDelete, fmt.Sprintf("/api2/repos/%s/file/", d.Addition.RepoId), func(req *resty.Request) {
+		req.SetQueryParams(map[string]string{
 			"p": obj.GetPath(),
-		}).
-		Delete(d.Address + fmt.Sprintf("/api2/repos/%s/file/", d.Addition.RepoId))
-
-	if resp.StatusCode() == 200 {
-		return nil
-	}
-
+		})
+	})
 	return err
 }
 
 func (d *Seafile) Put(ctx context.Context, dstDir model.Obj, stream model.FileStreamer, up driver.UpdateProgress) error {
-	resp, err := base.RestyClient.R().
-		SetHeader("Authorization", d.authorization).
-		SetQueryParams(map[string]string{
+	res, err := d.request(http.MethodGet, fmt.Sprintf("/api2/repos/%s/upload-link/", d.Addition.RepoId), func(req *resty.Request) {
+		req.SetQueryParams(map[string]string{
 			"p": dstDir.GetPath(),
-		}).
-		Get(d.Address + fmt.Sprintf("/api2/repos/%s/upload-link/", d.Addition.RepoId))
-
+		})
+	})
 	if err != nil {
 		return err
 	}
 
-	u := resp.String()
+	u := string(res)
 	u = u[1 : len(u)-1] // remove quotes
-
-	resp, err = base.RestyClient.R().
-		SetHeader("Authorization", d.authorization).
-		SetFileReader("file", stream.GetName(), stream).
-		SetFormData(map[string]string{
-			"parent_dir": dstDir.GetPath(),
-			"replace":    "1",
-		}).
-		Post(u)
-
-	if resp.StatusCode() == 200 {
-		return nil
-	}
+	_, err = d.request(http.MethodPost, u, func(req *resty.Request) {
+		req.SetFileReader("file", stream.GetName(), stream).
+			SetFormData(map[string]string{
+				"parent_dir": dstDir.GetPath(),
+				"replace":    "1",
+			})
+	})
 	return err
 }
 
