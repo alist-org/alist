603681fbe6bb2620daaddc49a10019e1c5a25d71
itsHenry <2671230065@qq.com>
2023-03-02 17:55:33 +0800

    feat: rebuild Single sign-on system (#3649 close #3571)
    * rebuild single sign on system
    
    * perf: use cache
    
    * fix: codefactor check
    
    ---------
    
    Co-authored-by: Andy Hsu <i@nn.ci>

--- internal/conf/const.go
+++ internal/conf/const.go
@@ -1,33 +1,33 @@
 package conf
 
 const (
-    TypeString = "string"
-    TypeSelect = "select"
-    TypeBool   = "bool"
-    TypeText   = "text"
-    TypeNumber = "number"
+	TypeString = "string"
+	TypeSelect = "select"
+	TypeBool   = "bool"
+	TypeText   = "text"
+	TypeNumber = "number"
 )
 
 const (
-    // site
-    VERSION      = "version"
-    SiteTitle    = "site_title"
-    Announcement = "announcement"
-    AllowIndexed = "allow_indexed"
+	// site
+	VERSION      = "version"
+	SiteTitle    = "site_title"
+	Announcement = "announcement"
+	AllowIndexed = "allow_indexed"
 
-    Logo      = "logo"
-    Favicon   = "favicon"
-    MainColor = "main_color"
+	Logo      = "logo"
+	Favicon   = "favicon"
+	MainColor = "main_color"
 
-    // preview
-    TextTypes          = "text_types"
-    AudioTypes         = "audio_types"
-    VideoTypes         = "video_types"
-    ImageTypes         = "image_types"
-    ProxyTypes         = "proxy_types"
-    ProxyIgnoreHeaders = "proxy_ignore_headers"
-    AudioAutoplay      = "audio_autoplay"
-    VideoAutoplay      = "video_autoplay"
+	// preview
+	TextTypes          = "text_types"
+	AudioTypes         = "audio_types"
+	VideoTypes         = "video_types"
+	ImageTypes         = "image_types"
+	ProxyTypes         = "proxy_types"
+	ProxyIgnoreHeaders = "proxy_ignore_headers"
+	AudioAutoplay      = "audio_autoplay"
+	VideoAutoplay      = "video_autoplay"
 
 	// global
 	HideFiles               = "hide_files"
@@ -39,36 +39,37 @@ const (
 	OcrApi                  = "ocr_api"
 	FilenameCharMapping     = "filename_char_mapping"
 	ForwardDirectLinkParams = "forward_direct_link_params"
-	
+
 	// index
 	SearchIndex     = "search_index"
 	AutoUpdateIndex = "auto_update_index"
 	IgnorePaths     = "ignore_paths"
 	MaxIndexDepth   = "max_index_depth"
 
-    // aria2
-    Aria2Uri    = "aria2_uri"
-    Aria2Secret = "aria2_secret"
+	// aria2
+	Aria2Uri    = "aria2_uri"
+	Aria2Secret = "aria2_secret"
 
-    // single
-    Token         = "token"
-    IndexProgress = "index_progress"
+	// single
+	Token         = "token"
+	IndexProgress = "index_progress"
 
-    //Github
-    GithubClientId      = "github_client_id"
-    GithubClientSecrets = "github_client_secrets"
-    GithubLoginEnabled  = "github_login_enabled"
+	//SSO
+	SSOClientId      = "sso_client_id"
+	SSOClientSecret  = "sso_client_secret"
+	SSOLoginEnabled  = "sso_login_enabled"
+	SSOLoginplatform = "sso_login_platform"
 
-    // qbittorrent
-    QbittorrentUrl = "qbittorrent_url"
+	// qbittorrent
+	QbittorrentUrl = "qbittorrent_url"
 )
 
 const (
-    UNKNOWN = iota
-    FOLDER
-    //OFFICE
-    VIDEO
-    AUDIO
-    TEXT
-    IMAGE
+	UNKNOWN = iota
+	FOLDER
+	//OFFICE
+	VIDEO
+	AUDIO
+	TEXT
+	IMAGE
 )
