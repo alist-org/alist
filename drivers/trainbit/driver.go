3c627fde0d0ac61c2c43f07527c74c92e89c9f37
NightSpaceC <NightSpaceC@outlook.com>
2023-03-11 12:49:06 +0800

    feat: add the support for Trainbit modify the structure of code allow to create folder, move, rename and remove

--- drivers/trainbit/driver.go
+++ drivers/trainbit/driver.go
@@ -2,7 +2,13 @@ package trainbit
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/alist-org/alist/v3/internal/driver"
 	"github.com/alist-org/alist/v3/internal/errs"
@@ -23,7 +29,6 @@ func (d *Trainbit) GetAddition() driver.Additional {
 }
 
 func (d *Trainbit) Init(ctx context.Context) error {
-	//op.MustSaveDriverStorage(d)
 	http.DefaultClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
         return http.ErrUseLastResponse
     }
@@ -35,55 +40,76 @@ func (d *Trainbit) Drop(ctx context.Context) error {
 }
 
 func (d *Trainbit) List(ctx context.Context, dir model.Obj, args model.ListArgs) ([]model.Obj, error) {
-	objectList, err := readFolder(dir.GetID(), d.AUSHELLPORTAL, d.ApiKey)
+	form := make(url.Values)
+	form.Set("parentid", strings.Split(dir.GetID(), "_")[0])
+	res, err := postForm("https://trainbit.com/lib/api/v1/listoffiles", form, d.ApiKey, d.AUSHELLPORTAL)
 	if err != nil {
 		return nil, err
 	}
-	return objectList, nil
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	var jsonData any
+	json.Unmarshal(data, &jsonData)
+	if err != nil {
+		return nil, err
+	}
+	object, err := parseRawFileObject(jsonData.(map[string]any)["items"].([]any))
+	if err != nil {
+		return nil, err
+	}
+	return object, nil
 }
 
 func (d *Trainbit) Link(ctx context.Context, file model.Obj, args model.LinkArgs) (*model.Link, error) {
-	link, err := getDownloadLink(file.GetID(), d.AUSHELLPORTAL)
+	res, err := get(fmt.Sprintf("https://trainbit.com/files/%s/", strings.Split(file.GetID(), "_")[0]), d.AUSHELLPORTAL)
 	if err != nil {
 		return nil, err
 	}
 	return &model.Link{
-		URL: link,
+		URL: res.Header.Get("Location"),
 	}, nil
 }
 
 func (d *Trainbit) MakeDir(ctx context.Context, parentDir model.Obj, dirName string) error {
-	// TODO create folder
-	return errs.NotImplement
+	form := make(url.Values)
+	form.Set("name", base64.URLEncoding.EncodeToString([]byte(dirName)))
+	form.Set("parentid", strings.Split(parentDir.GetID(), "_")[0])
+	_, err := postForm("https://trainbit.com/lib/api/v1/createfolder", form, d.ApiKey, d.AUSHELLPORTAL)
+	return err
 }
 
 func (d *Trainbit) Move(ctx context.Context, srcObj, dstDir model.Obj) error {
-	// TODO move obj
-	return errs.NotImplement
+	form := make(url.Values)
+	form.Set("sourceid", strings.Split(srcObj.GetID(), "_")[0])
+	form.Set("destinationid", strings.Split(dstDir.GetID(), "_")[0])
+	_, err := postForm("https://trainbit.com/lib/api/v1/move", form, d.ApiKey, d.AUSHELLPORTAL)
+	return err
 }
 
 func (d *Trainbit) Rename(ctx context.Context, srcObj model.Obj, newName string) error {
-	// TODO rename obj
-	return errs.NotImplement
+	form := make(url.Values)
+	form.Set("id", strings.Split(srcObj.GetID(), "_")[0])
+	form.Set("name", base64.URLEncoding.EncodeToString([]byte(newName)))
+	_, err := postForm("https://trainbit.com/lib/api/v1/edit", form, d.ApiKey, d.AUSHELLPORTAL)
+	return err
 }
 
 func (d *Trainbit) Copy(ctx context.Context, srcObj, dstDir model.Obj) error {
-	// TODO copy obj
 	return errs.NotImplement
 }
 
 func (d *Trainbit) Remove(ctx context.Context, obj model.Obj) error {
-	// TODO remove obj
-	return errs.NotImplement
+	form := make(url.Values)
+	form.Set("id", strings.Split(obj.GetID(), "_")[0])
+	_, err := postForm("https://trainbit.com/lib/api/v1/delete", form, d.ApiKey, d.AUSHELLPORTAL)
+	return err
 }
 
 func (d *Trainbit) Put(ctx context.Context, dstDir model.Obj, stream model.FileStreamer, up driver.UpdateProgress) error {
 	// TODO upload file
 	return errs.NotImplement
 }
-
-//func (d *Trainbit) Other(ctx context.Context, args model.OtherArgs) (interface{}, error) {
-//	return nil, errs.NotSupport
-//}
 
 var _ driver.Driver = (*Trainbit)(nil)
--- drivers/trainbit/util.go
+++ drivers/trainbit/util.go
@@ -2,9 +2,6 @@ package trainbit
 
 import (
 	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -13,7 +10,7 @@ import (
 	"github.com/alist-org/alist/v3/internal/model"
 )
 
-func Get(url string, AUSHELLPORTAL string) (*http.Response, error) {
+func get(url string, AUSHELLPORTAL string) (*http.Response, error) {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
@@ -27,10 +24,15 @@ func Get(url string, AUSHELLPORTAL string) (*http.Response, error) {
 	return res, err
 }
 
-func PostForm(url string, data url.Values, apiKey string, AUSHELLPORTAL string) (*http.Response, error) {
-	data.Add("apikey", apiKey)
-	data.Add("expiredate", "0001-01-06")
-	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(data.Encode()))
+func postForm(endpoint string, data url.Values, apiKey string, AUSHELLPORTAL string) (*http.Response, error) {
+	extData := make(url.Values)
+	for key, value := range data {
+		extData[key] = make([]string, len(value))
+		copy(extData[key], value)
+	}
+	extData.Set("apikey", apiKey)
+	extData.Set("expiredate", "0001-01-06")
+	req, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(extData.Encode()))
 	if err != nil {
 		return nil, err
 	}
@@ -44,32 +46,9 @@ func PostForm(url string, data url.Values, apiKey string, AUSHELLPORTAL string) 
 	return res, err
 }
 
-func getDownloadLink(id string, AUSHELLPORTAL string) (string, error) {
-	res, err := Get(fmt.Sprintf("https://trainbit.com/files/%s/", strings.Split(id, "_")[0]), AUSHELLPORTAL)
-	if err != nil {
-		return "", err
-	}
-	return res.Header.Get("Location"), nil
-}
-
-func readFolder(id string, AUSHELLPORTAL string, apiKey string) ([]model.Obj, error) {
-	form := make(url.Values)
-	form.Add("parentid", strings.Split(id, "_")[0])
-	res, err := PostForm("https://trainbit.com/lib/api/v1/listoffiles", form, apiKey, AUSHELLPORTAL)
-	if err != nil {
-		return nil, err
-	}
-	rawData, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	content := make(map[string]any)
-	err = json.Unmarshal(rawData, &content)
-	if err != nil {
-		return nil, err
-	}
+func parseRawFileObject(rawObject []any) ([]model.Obj, error) {
 	objectList := make([]model.Obj, 0)
-	for _, each := range content["items"].([]any) {
+	for _, each := range rawObject {
 		object := each.(map[string]any)
 		if object["id"].(string) == "0" {
 			continue
@@ -92,30 +71,3 @@ func readFolder(id string, AUSHELLPORTAL string, apiKey string) ([]model.Obj, er
 	}
 	return objectList, nil
 }
-
-// func getObjectId(path string, AUSHELLPORTAL string, rootId string) ([]string, error) {
-// 	path = filepath.Clean(path)
-// 	part := strings.Split(path, "/")
-// 	if part[0] == "." {
-// 		return []string{rootId}, nil
-// 	}
-// 	objectId := []string{rootId}
-// 	for index, each := range part {
-// 		objectList, err := readFolder(objectId[len(objectId) - 1], AUSHELLPORTAL)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		flag := false
-// 		for _, object := range objectList {
-// 			if object.Name == each && (index == len(part) - 1 && !object.IsFolder) {
-// 				objectId = append(objectId, object.Id)
-// 				flag = true
-// 				break
-// 			}
-// 		}
-// 		if !flag {
-// 			return nil, errors.New("not found")
-// 		}
-// 	}
-// 	return objectId, nil
-// }
