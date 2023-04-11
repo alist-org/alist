41d7699110698662fed62a5d38d27d061e5d80cc
Andy Hsu <i@nn.ci>
2023-04-11 20:01:50 +0800

    base tool

---
+++ internal/offline_download/util.go
@@ -0,0 +1,27 @@
+package offline_download
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func GetFiles(dir string) ([]*File, error) {
+	var files []*File
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			files = append(files, &File{
+				Name: info.Name(),
+				Size: info.Size(),
+				Path: path,
+			})
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return files, nil
+}
