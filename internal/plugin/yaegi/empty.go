cd0c773ee70e957383bd1b40b10e1dad2c4a0947
foxxorcat <foxxorcat@foxmail.com>
2023-04-23 01:25:45 +0800

    chore: add plugin build tag

---
+++ internal/plugin/yaegi/empty.go
@@ -0,0 +1,1 @@
+package plugin_yaegi
--- internal/plugin/yaegi/yaegi.go
+++ internal/plugin/yaegi/yaegi.go
@@ -1,7 +1,11 @@
+//go:build yaegi_mode
+// +build yaegi_mode
+
 package plugin_yaegi
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/alist-org/alist/v3/internal/conf"
 	"github.com/alist-org/alist/v3/internal/model"
@@ -13,6 +17,13 @@ import (
 )
 
 func LoadYaegiPlugin(plugin model.Plugin) (model.PluginControl, error) {
+	var err error
+	if !filepath.IsAbs(conf.Conf.PluginDir) {
+		conf.Conf.PluginDir, err = filepath.Abs(conf.Conf.PluginDir)
+		if err != nil {
+			return nil, err
+		}
+	}
 	i := interp.New(interp.Options{GoPath: conf.Conf.PluginDir})
 	// 插件需要使用标准库
 	if err := i.Use(stdlib.Symbols); err != nil {
@@ -25,7 +36,7 @@ func LoadYaegiPlugin(plugin model.Plugin) (model.PluginControl, error) {
 	}
 
 	// 导出 PluginControl
-	_, err := i.Eval(fmt.Sprintf(`import p "%s"`, plugin.Path))
+	_, err = i.Eval(fmt.Sprintf(`import p "%s"`, plugin.Path))
 	if err != nil {
 		return nil, err
 	}
