4dca52be85373f6adbf3e9efc46a7b15f25d7be4
Andy Hsu <i@nn.ci>
2023-06-06 22:47:27 +0800

    fix(s3): optional add filename to disposition (close #4538)

--- drivers/s3/meta.go
+++ drivers/s3/meta.go
@@ -7,17 +7,18 @@ import (
 
 type Addition struct {
 	driver.RootPath
-	Bucket            string `json:"bucket" required:"true"`
-	Endpoint          string `json:"endpoint" required:"true"`
-	Region            string `json:"region"`
-	AccessKeyID       string `json:"access_key_id" required:"true"`
-	SecretAccessKey   string `json:"secret_access_key" required:"true"`
-	CustomHost        string `json:"custom_host"`
-	SignURLExpire     int    `json:"sign_url_expire" type:"number" default:"4"`
-	Placeholder       string `json:"placeholder"`
-	ForcePathStyle    bool   `json:"force_path_style"`
-	ListObjectVersion string `json:"list_object_version" type:"select" options:"v1,v2" default:"v1"`
-	RemoveBucket      bool   `json:"remove_bucket" help:"Remove bucket name from path when using custom host."`
+	Bucket                   string `json:"bucket" required:"true"`
+	Endpoint                 string `json:"endpoint" required:"true"`
+	Region                   string `json:"region"`
+	AccessKeyID              string `json:"access_key_id" required:"true"`
+	SecretAccessKey          string `json:"secret_access_key" required:"true"`
+	CustomHost               string `json:"custom_host"`
+	SignURLExpire            int    `json:"sign_url_expire" type:"number" default:"4"`
+	Placeholder              string `json:"placeholder"`
+	ForcePathStyle           bool   `json:"force_path_style"`
+	ListObjectVersion        string `json:"list_object_version" type:"select" options:"v1,v2" default:"v1"`
+	RemoveBucket             bool   `json:"remove_bucket" help:"Remove bucket name from path when using custom host."`
+	AddFilenameToDisposition bool   `json:"add_filename_to_disposition" help:"Add filename to Content-Disposition header."`
 }
 
 var config = driver.Config{
