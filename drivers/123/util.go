02dbc288ef5ee2d96c22c5c6e355dd10af8c3326
tangrui <tr@geektang.cn>
2023-07-21 00:09:21 +0800

    fix: add authKey verification for 123pan

--- drivers/123/util.go
+++ drivers/123/util.go
@@ -1,15 +1,18 @@
 package _123
 
 import (
+	"crypto/md5"
 	"errors"
 	"fmt"
-	"net/http"
-	"strconv"
-
 	"github.com/alist-org/alist/v3/drivers/base"
 	"github.com/alist-org/alist/v3/pkg/utils"
 	"github.com/go-resty/resty/v2"
 	jsoniter "github.com/json-iterator/go"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
 )
 
 // do others that not defined in Driver interface
@@ -17,7 +20,7 @@ import (
 const (
 	AApi             = "https://www.123pan.com/a/api"
 	BApi             = "https://www.123pan.com/b/api"
-	MainApi          = AApi
+	MainApi          = BApi
 	SignIn           = MainApi + "/user/sign_in"
 	Logout           = MainApi + "/user/logout"
 	UserInfo         = MainApi + "/user/info"
@@ -33,6 +36,7 @@ const (
 	S3Auth           = MainApi + "/file/s3_upload_object/auth"
 	UploadCompleteV2 = MainApi + "/file/upload_complete/v2"
 	S3Complete       = MainApi + "/file/s3_complete_multipart_upload"
+	AuthKeySalt      = "8-8D$sL8gPjom7bk#cY"
 )
 
 func (d *Pan123) login() error {
@@ -70,6 +74,20 @@ func (d *Pan123) login() error {
 	return err
 }
 
+func authKey(reqUrl string) (*string, error) {
+	reqURL, err := url.Parse(reqUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	nowUnix := time.Now().Unix()
+	random := rand.Intn(0x989680)
+
+	p4 := fmt.Sprintf("%d|%d|%s|%s|%s|%s", nowUnix, random, reqURL.Path, "web", "3", AuthKeySalt)
+	authKey := fmt.Sprintf("%d-%d-%x", nowUnix, random, md5.Sum([]byte(p4)))
+	return &authKey, nil
+}
+
 func (d *Pan123) request(url string, method string, callback base.ReqCallback, resp interface{}) ([]byte, error) {
 	req := base.RestyClient.R()
 	req.SetHeaders(map[string]string{
@@ -80,12 +98,15 @@ func (d *Pan123) request(url string, method string, callback base.ReqCallback, r
 		"app-version":   "3",
 		"user-agent":    base.UserAgent,
 	})
+	authKey, err := authKey(url)
+
 	if callback != nil {
 		callback(req)
 	}
 	if resp != nil {
 		req.SetResult(resp)
 	}
+	req.SetQueryParam("auth-key", *authKey)
 	res, err := req.Execute(method, url)
 	if err != nil {
 		return nil, err
