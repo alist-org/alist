1851c735c0f0ce7e407f149228d919038dd2016e
foxxorcat <foxxorcat@foxmail.com>
2023-08-08 23:14:17 +0800

    feat(weiyun):multi threaded upload

--- drivers/weiyun/meta.go
+++ drivers/weiyun/meta.go
@@ -10,6 +10,7 @@ type Addition struct {
 	Cookies        string `json:"cookies" required:"true"`
 	OrderBy        string `json:"order_by" type:"select" options:"name,size,updated_at" default:"name"`
 	OrderDirection string `json:"order_direction" type:"select" options:"asc,desc" default:"asc"`
+	UploadThread   string `json:"upload_thread" default:"4" help:"4<=thread<=32"`
 }
 
 var config = driver.Config{
