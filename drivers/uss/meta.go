b05d3cb0a224613d7fc9fe462d651d04f6f72d3c
萌萌哒赫萝 <ma_shiqing@163.com>
2023-08-29 01:30:42 -0700

    feat(uss): add AntiTheftChainToken; fix link func

--- drivers/uss/meta.go
+++ drivers/uss/meta.go
@@ -7,10 +7,11 @@ import (
 
 type Addition struct {
 	driver.RootPath
-	Bucket           string `json:"bucket" required:"true"`
-	Endpoint         string `json:"endpoint" required:"true"`
-	OperatorName     string `json:"operator_name" required:"true"`
-	OperatorPassword string `json:"operator_password" required:"true"`
+	Bucket              string `json:"bucket" required:"true"`
+	Endpoint            string `json:"endpoint" required:"true"`
+	OperatorName        string `json:"operator_name" required:"true"`
+	OperatorPassword    string `json:"operator_password" required:"true"`
+	AntiTheftChainToken string `json:"anti_theft_chain_token" required:"false" default:""`
 	//CustomHost       string `json:"custom_host"`	//Endpoint与CustomHost作用相同，去除
 	SignURLExpire int `json:"sign_url_expire" type:"number" default:"4"`
 }
