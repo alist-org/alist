fea04f80892e529db85b57c7e1e0c73038aa5e96
Nick Wang <chaoqing@users.noreply.github.com>
2023-09-25 04:05:25 +0000

    feat: crypt driver can load pre-generated thumbnails under the subdirectory

--- drivers/crypt/meta.go
+++ drivers/crypt/meta.go
@@ -19,6 +19,8 @@ type Addition struct {
 	Salt             string `json:"salt" confidential:"true"  help:"If you don't know what is salt, treat it as a second password. Optional but recommended"`
 	EncryptedSuffix  string `json:"encrypted_suffix" required:"true" default:".bin" help:"for advanced user only! encrypted files will have this suffix"`
 	FileNameEncoding string `json:"filename_encoding" type:"select" required:"true" options:"base64,base32,base32768" default:"base64" help:"for advanced user only!"`
+
+	Thumbnail   bool   `json:"thumbnail" required:"true" default:"false" help:"enable thumbnail which pre-generated under .thumbnails folder"`
 }
 
 var config = driver.Config{
