4355dae491ba251ce99826ad8833a47ed8cbdad5
Andy Hsu <i@nn.ci>
2023-11-06 18:20:25 +0800

    fix: incorrect content-type of apk files (close #5385)

--- pkg/utils/file.go
+++ pkg/utils/file.go
@@ -163,8 +163,15 @@ func GetObjType(filename string, isDir bool) int {
 	return GetFileType(filename)
 }
 
+var extraMimeTypes = map[string]string{
+	".apk": "application/vnd.android.package-archive",
+}
+
 func GetMimeType(name string) string {
 	ext := path.Ext(name)
+	if m, ok := extraMimeTypes[ext]; ok {
+		return m
+	}
 	m := mime.TypeByExtension(ext)
 	if m != "" {
 		return m
--- server/common/proxy.go
+++ server/common/proxy.go
@@ -76,4 +76,5 @@ func Proxy(w http.ResponseWriter, r *http.Request, link *model.Link, file model.
 func attachFileName(w http.ResponseWriter, file model.Obj) {
 	fileName := file.GetName()
 	w.Header().Set("Content-Disposition", fmt.Sprintf(`attachment; filename="%s"; filename*=UTF-8''%s`, fileName, url.PathEscape(fileName)))
+	w.Header().Set("Content-Type", utils.GetMimeType(fileName))
 }
