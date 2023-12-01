20f06353710f8abdc61db8da8ccb18db1305827a
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-12-01 15:20:05 +0800

    fix(aliyundrive_open): New API Domain From Official Docs to Support More IPs and IPV6
    API From https://openapi.aliyundrive.com to https://openapi.alipan.com 
    原域名是 101.201.69.253 单点
    新域名是三大运营商多点+海外阿里云+IPV6支持（北京阿里云2点）
    
    可以改善 IPV6网络的异常分享
    以及不同运营商访问的稳定性
    
    以及官方在  https://www.yuque.com/aliyundrive/zpfszx/rgg2p1qnsfdux61r 确实指出 接口域名为：https://openapi.alipan.com

--- drivers/aliyundrive_open/meta.go
+++ drivers/aliyundrive_open/meta.go
@@ -36,7 +36,7 @@ var config = driver.Config{
 func init() {
 	op.RegisterDriver(func() driver.Driver {
 		return &AliyundriveOpen{
-			base: "https://openapi.aliyundrive.com",
+			base: "https://openapi.alipan.com",
 		}
 	})
 }
