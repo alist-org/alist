c3c518184706e195ea14d127b726c73bad4a4a49
tukipona <161995125+tukipona@users.noreply.github.com>
2024-04-10 21:50:30 +0800

    feat(Seafile): add token login (#6324 close #5302)

--- drivers/seafile/meta.go
+++ drivers/seafile/meta.go
@@ -9,8 +9,9 @@ type Addition struct {
 	driver.RootPath
 
 	Address  string `json:"address" required:"true"`
-	UserName string `json:"username" required:"true"`
-	Password string `json:"password" required:"true"`
+	UserName string `json:"username" required:"false"`
+	Password string `json:"password" required:"false"`
+	Token    string `json:"token" required:"false"`	
 	RepoId   string `json:"repoId" required:"false"`
 	RepoPwd  string `json:"repoPwd" required:"false"`
 }
