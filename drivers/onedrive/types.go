583ca7803c68994f4134d18f53e919432e792b38
potoo0 <1415615232@qq.com>
2024-04-26 16:06:06 +0800

    fix(onedrive) fix Ctime/Mtime

--- drivers/onedrive/types.go
+++ drivers/onedrive/types.go
@@ -24,12 +24,12 @@ type RespErr struct {
 }
 
 type File struct {
-	Id                   string    `json:"id"`
-	Name                 string    `json:"name"`
-	Size                 int64     `json:"size"`
-	LastModifiedDateTime time.Time `json:"lastModifiedDateTime"`
-	Url                  string    `json:"@microsoft.graph.downloadUrl"`
-	File                 *struct {
+	Id             string               `json:"id"`
+	Name           string               `json:"name"`
+	Size           int64                `json:"size"`
+	FileSystemInfo *FileSystemInfoFacet `json:"fileSystemInfo"`
+	Url            string               `json:"@microsoft.graph.downloadUrl"`
+	File           *struct {
 		MimeType string `json:"mimeType"`
 	} `json:"file"`
 	Thumbnails []struct {
@@ -58,7 +58,7 @@ func fileToObj(f File, parentID string) *Object {
 				ID:       f.Id,
 				Name:     f.Name,
 				Size:     f.Size,
-				Modified: f.LastModifiedDateTime,
+				Modified: f.FileSystemInfo.LastModifiedDateTime,
 				IsFolder: f.File == nil,
 			},
 			Thumbnail: model.Thumbnail{Thumbnail: thumb},
@@ -71,4 +71,21 @@ func fileToObj(f File, parentID string) *Object {
 type Files struct {
 	Value    []File `json:"value"`
 	NextLink string `json:"@odata.nextLink"`
+}
+
+// Metadata represents a request to update Metadata.
+// It includes only the writeable properties.
+// omitempty is intentionally included for all, per https://learn.microsoft.com/en-us/onedrive/developer/rest-api/api/driveitem_update?view=odsp-graph-online#request-body
+type Metadata struct {
+	Description    string               `json:"description,omitempty"`    // Provides a user-visible description of the item. Read-write. Only on OneDrive Personal. Undocumented limit of 1024 characters.
+	FileSystemInfo *FileSystemInfoFacet `json:"fileSystemInfo,omitempty"` // File system information on client. Read-write.
+}
+
+// FileSystemInfoFacet contains properties that are reported by the
+// device's local file system for the local version of an item. This
+// facet can be used to specify the last modified date or created date
+// of the item as it was on the local device.
+type FileSystemInfoFacet struct {
+	CreatedDateTime      time.Time `json:"createdDateTime,omitempty"`      // The UTC date and time the file was created on a client.
+	LastModifiedDateTime time.Time `json:"lastModifiedDateTime,omitempty"` // The UTC date and time the file was last modified on a client.
 }
