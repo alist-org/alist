3c6650a58c71ee2ac2db8e63bff7e6941aacd896
Kuingsmile <ma_shiqing@163.com>
2024-05-19 15:44:36 +0800

    feat(mega): add support for two-factor authentication in Mega driver  #6226

--- drivers/mega/meta.go
+++ drivers/mega/meta.go
@@ -9,8 +9,10 @@ type Addition struct {
 	// Usually one of two
 	//driver.RootPath
 	//driver.RootID
-	Email    string `json:"email" required:"true"`
-	Password string `json:"password" required:"true"`
+	Email       string `json:"email" required:"true"`
+	Password    string `json:"password" required:"true"`
+	TwoFACode   string `json:"two_fa_code" required:"false"`
+	TwoFASecret string `json:"two_fa_secret" required:"false"`
 }
 
 var config = driver.Config{
