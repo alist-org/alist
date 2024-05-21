bbe3d4e19f6171154b14a5b65fe527007e655607
YangXu <47767754+Three-taile-dragon@users.noreply.github.com>
2024-05-21 23:24:28 +0800

    feat: add supports for thunderX driver (#6464)

---
+++ drivers/thunderx/meta.go
@@ -0,0 +1,103 @@
+package thunderx
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+
+	"github.com/alist-org/alist/v3/internal/driver"
+	"github.com/alist-org/alist/v3/internal/op"
+	"github.com/alist-org/alist/v3/pkg/utils"
+)
+
+// 高级设置
+type ExpertAddition struct {
+	driver.RootID
+
+	LoginType string `json:"login_type" type:"select" options:"user,refresh_token" default:"user"`
+	SignType  string `json:"sign_type" type:"select" options:"algorithms,captcha_sign" default:"algorithms"`
+
+	// 登录方式1
+	Username string `json:"username" required:"true" help:"login type is user,this is required"`
+	Password string `json:"password" required:"true" help:"login type is user,this is required"`
+	// 登录方式2
+	RefreshToken string `json:"refresh_token" required:"true" help:"login type is refresh_token,this is required"`
+
+	// 签名方法1
+	Algorithms string `json:"algorithms" required:"true" help:"sign type is algorithms,this is required" default:"lHwINjLeqssT28Ym99p5MvR,xvFcxvtqPKCa9Ajf,2ywOP8spKHzfuhZMUYZ9IpsViq0t8vT0,FTBrJism20SHKQ2m2,BHrWJsPwjnr5VeLtOUr2191X9uXhWmt,yu0QgHEjNmDoPNwXN17so2hQlDT83T,OcaMfLMCGZ7oYlvZGIbTqb4U7cCY,jBGGu0GzXOjtCXYwkOBb+c6TZ/Nymv,YLWRjVor2rOuYEL,94wjoPazejyNC+gRpOj+JOm1XXvxa"`
+	// 签名方法2
+	CaptchaSign string `json:"captcha_sign" required:"true" help:"sign type is captcha_sign,this is required"`
+	Timestamp   string `json:"timestamp" required:"true" help:"sign type is captcha_sign,this is required"`
+
+	// 验证码
+	CaptchaToken string `json:"captcha_token"`
+
+	// 必要且影响登录,由签名决定
+	DeviceID      string `json:"device_id"  required:"true" default:"9aa5c268e7bcfc197a9ad88e2fb330e5"`
+	ClientID      string `json:"client_id"  required:"true" default:"ZQL_zwA4qhHcoe_2"`
+	ClientSecret  string `json:"client_secret"  required:"true" default:"Og9Vr1L8Ee6bh0olFxFDRg"`
+	ClientVersion string `json:"client_version"  required:"true" default:"1.05.0.2115"`
+	PackageName   string `json:"package_name"  required:"true" default:"com.thunder.downloader"`
+
+	//不影响登录,影响下载速度
+	UserAgent         string `json:"user_agent"  required:"true" default:"ANDROID-com.thunder.downloader/1.05.0.2115 netWorkType/4G appid/40 deviceName/Xiaomi_M2004j7ac deviceModel/M2004J7AC OSVersion/12 protocolVersion/301 platformVersion/10 sdkVersion/220200 Oauth2Client/0.9 (Linux 4_14_186-perf-gdcf98eab238b) (JAVA 0)"`
+	DownloadUserAgent string `json:"download_user_agent"  required:"true" default:"Dalvik/2.1.0 (Linux; U; Android 12; M2004J7AC Build/SP1A.210812.016)"`
+
+	//优先使用视频链接代替下载链接
+	UseVideoUrl bool `json:"use_video_url"`
+}
+
+// 登录特征,用于判断是否重新登录
+func (i *ExpertAddition) GetIdentity() string {
+	hash := md5.New()
+	if i.LoginType == "refresh_token" {
+		hash.Write([]byte(i.RefreshToken))
+	} else {
+		hash.Write([]byte(i.Username + i.Password))
+	}
+
+	if i.SignType == "captcha_sign" {
+		hash.Write([]byte(i.CaptchaSign + i.Timestamp))
+	} else {
+		hash.Write([]byte(i.Algorithms))
+	}
+
+	hash.Write([]byte(i.DeviceID))
+	hash.Write([]byte(i.ClientID))
+	hash.Write([]byte(i.ClientSecret))
+	hash.Write([]byte(i.ClientVersion))
+	hash.Write([]byte(i.PackageName))
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+type Addition struct {
+	driver.RootID
+	Username     string `json:"username" required:"true"`
+	Password     string `json:"password" required:"true"`
+	CaptchaToken string `json:"captcha_token"`
+	UseVideoUrl  bool   `json:"use_video_url" default:"true"`
+}
+
+// 登录特征,用于判断是否重新登录
+func (i *Addition) GetIdentity() string {
+	return utils.GetMD5EncodeStr(i.Username + i.Password)
+}
+
+var config = driver.Config{
+	Name:      "ThunderX",
+	LocalSort: true,
+	OnlyProxy: true,
+}
+
+var configExpert = driver.Config{
+	Name:      "ThunderXExpert",
+	LocalSort: true,
+}
+
+func init() {
+	op.RegisterDriver(func() driver.Driver {
+		return &ThunderX{}
+	})
+	op.RegisterDriver(func() driver.Driver {
+		return &ThunderXExpert{}
+	})
+}
