5ac061ade41ab1faa5a61e77640156ca87efbb74
Toby Shi <toby3030@qq.com>
2024-05-27 15:58:21 +0800

    feat(ftp): add ftp encoding support

--- drivers/ftp/meta.go
+++ drivers/ftp/meta.go
@@ -3,10 +3,28 @@ package ftp
 import (
 	"github.com/alist-org/alist/v3/internal/driver"
 	"github.com/alist-org/alist/v3/internal/op"
+	"github.com/axgle/mahonia"
 )
+
+func encode(str string, encoding string) string {
+	if encoding == "" {
+		return str
+	}
+	encoder := mahonia.NewEncoder(encoding)
+	return encoder.ConvertString(str)
+}
+
+func decode(str string, encoding string) string {
+	if encoding == "" {
+		return str
+	}
+	decoder := mahonia.NewDecoder(encoding)
+	return decoder.ConvertString(str)
+}
 
 type Addition struct {
 	Address  string `json:"address" required:"true"`
+	Encoding string `json:"encoding" required:"true"`
 	Username string `json:"username" required:"true"`
 	Password string `json:"password" required:"true"`
 	driver.RootPath
