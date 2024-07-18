2e73f5c36ac438481b64f28181d576f0f71b80cb
David Hao <akang943578@gmail.com>
2024-07-18 08:45:43 +0800

    Update alist-org/gofakes3 to v0.0.7, support create folder in PutObject.

--- server/s3/backend.go
+++ server/s3/backend.go
@@ -267,8 +267,19 @@ func (b *s3Backend) PutObject(
 	}
 	bucketPath := bucket.Path
 
+	isDir := strings.HasSuffix(objectName, "/")
+	log.Debugf("isDir: %v", isDir)
+
 	fp := path.Join(bucketPath, objectName)
-	reqPath := path.Dir(fp)
+	log.Debugf("fp: %s, bucketPath: %s, objectName: %s", fp, bucketPath, objectName)
+
+	var reqPath string
+	if isDir {
+		reqPath = fp + "/"
+	} else {
+		reqPath = path.Dir(fp)
+	}
+	log.Debugf("reqPath: %s", reqPath)
 	fmeta, _ := op.GetNearestMeta(fp)
 	ctx = context.WithValue(ctx, "meta", fmeta)
 
@@ -283,6 +294,10 @@ func (b *s3Backend) PutObject(
 		} else {
 			return result, gofakes3.KeyNotFound(objectName)
 		}
+	}
+
+	if isDir {
+		return result, nil
 	}
 
 	var ti time.Time
