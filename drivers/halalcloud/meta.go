d4285b7c6c28304d57c5833ccdf7adcdace2126e
YangXu <47767754+Three-taile-dragon@users.noreply.github.com>
2024-08-04 19:03:24 +0800

    fix(halalcloud): fix some custom fields not taking effect & update appID and appSecret (#6938)

--- drivers/halalcloud/meta.go
+++ drivers/halalcloud/meta.go
@@ -12,9 +12,9 @@ type Addition struct {
 	RefreshToken string `json:"refresh_token" required:"true" help:"login type is refresh_token,this is required"`
 	UploadThread string `json:"upload_thread" default:"3" help:"1 <= thread <= 32"`
 
-	AppID      string `json:"app_id" required:"true" default:"devDebugger/1.0"`
+	AppID      string `json:"app_id" required:"true" default:"alist/10001"`
 	AppVersion string `json:"app_version" required:"true" default:"1.0.0"`
-	AppSecret  string `json:"app_secret" required:"true" default:"Nkx3Y2xvZ2luLmNu"`
+	AppSecret  string `json:"app_secret" required:"true" default:"bR4SJwOkvnG5WvVJ"`
 }
 
 var config = driver.Config{
