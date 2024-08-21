d2514d236ffa93e30d3cb22ec9df4d03e10e1000
ice yao <yao3690093@gmail.com>
2024-08-22 00:46:38 +0800

    feat(drivers): add kodbox storage (#7059 close #7058)
    - kodbox: https://github.com/kalcaddle/kodbox

---
+++ drivers/kodbox/meta.go
@@ -0,0 +1,25 @@
+package kodbox
+
+import (
+	"github.com/alist-org/alist/v3/internal/driver"
+	"github.com/alist-org/alist/v3/internal/op"
+)
+
+type Addition struct {
+	driver.RootPath
+
+	Address  string `json:"address" required:"true"`
+	UserName string `json:"username" required:"false"`
+	Password string `json:"password" required:"false"`
+}
+
+var config = driver.Config{
+	Name:        "KodBox",
+	DefaultRoot: "",
+}
+
+func init() {
+	op.RegisterDriver(func() driver.Driver {
+		return &KodBox{}
+	})
+}
